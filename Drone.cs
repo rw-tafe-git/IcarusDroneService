caa96265dffb2bf464c680d59f957a384ed31ff6
30002737 <30002737@tafe.wa.edu.au>
2023-05-19 10:10:43 +0800

    Updated progress

---
+++ Drone.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcarusDroneService
+{
+    internal class Drone
+    {
+        /*private string ClientName;
+        private string DroneModel;
+        private string ServiceProblem;
+        //private string ClientName;
+        //private string ClientName;
+
+        public Drone() { }
+
+        public string GetClientName()
+        {
+            return ClientName;
+        }
+
+        public void SetClientName(string newClient)
+        {
+            
+        }
+
+        public string GetClientName()
+        {
+            return ClientName;
+        }
+
+        public void SetClientName(string newClient)
+        {
+
+        }
+
+        public string GetClientName()
+        {
+            return ClientName;
+        }
+
+        public void SetServiceCost(string newClient)
+        {
+            if (newServiceCost <= 0)
+                ServiceCost = 44.99;
+            else
+                ServiceCost = newServiceCost;
+
+        }*/
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,80 @@ namespace IcarusDroneService
         {
             InitializeComponent();
         }
+
+        //private List<Drone> FinishedList;
+
+
+        // private void ButtonAddService_PreviewMouseDown()
+
+        private int GetServicePriority()
+        {
+            int priority = 0;
+            if(rbRegular.IsChecked == true)
+            {
+
+            }
+
+            return priority;
+        }
+
+        private void DisplayRegularService()
+        {
+            /*listViewRegularServiceQueue.Items.Clear();
+
+            foreach (Drone value in RegularService)
+            {
+
+                listViewRegularServiceQueue.Items.Add(new { Name = value.GetClientName, Model = value.GetClientName, Problem = value.GetClientName, Cost = value.GetClientName, Tag = value.GetClientName })
+            }*/
+        }
+
+        private void TextBoxServiceCost_PreviewTextInput()
+        {
+            //Regex regex = new Regex
+        }
+
+        //private void IncrementS
+
+        /*private void listViewRegularServiceQueue_SelectionChanged()
+        {
+            if (listViewRegularServiceQueue.SelectedIndex != -1)
+            {
+                int index = listViewRegularServiceQueue.SelectedIndex;
+                TextBoxClientName.Text = RegularService.ElementAt(index).GetClientName();
+                TextBoxClientModel.Text = RegularService.ElementAt(index).GetClientModel();
+                TextBoxServiceCost.Text = RegularService.ElementAt(index).GetServiceCost();
+                TextBoxServiceProblem.Text = RegularService.ElementAt(index).GetServiceProblem();
+            }
+            else
+            {
+                listViewRegularServiceQueue.UnselectAll();
+            }
+        }
+
+        private void RegularServiceDrone_PreviewMouseDown()
+        {
+            if (RegularService.Count > 0)
+            {
+                FinishedList.Add(ExpressService.Dequeue());
+                DisplayExpressService();
+                FinishedListDisplay();
+            }
+        }
+
+        private void FinishedListDisplay()
+        {
+            listBoxFinished.Items.Clear();
+            foreach(Drone drone in FinishedList)
+            {
+                listBoxFinished.Items.Add(drone.DisplayFinishedDrones());
+            }
+        }
+
+        private void listBoxFinished_MouseDoubleClick()
+        {
+            int index = listBoxFinished.SelectedIndex;
+            FinishedListDisplay().RemoveAt()
+        }*/
     }
 }
